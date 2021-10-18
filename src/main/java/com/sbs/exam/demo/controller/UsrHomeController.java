8affe720d470e5ba3e30892669925170cce1530f
hyun0547 <hyun0547@gmail.com>
2021-10-18 21:06:33 +0900

    count조절 액션 구현

--- src/main/java/com/sbs/exam/demo/controller/UsrHomeController.java
+++ src/main/java/com/sbs/exam/demo/controller/UsrHomeController.java
@@ -10,31 +10,20 @@ import lombok.Data;
 @Controller
 public class UsrHomeController {
 	private int count = 0;
-	@RequestMapping("/usr/home/main")
-	@ResponseBody
-	public String showMain() {
-		return "안녕하세요";
-	}
-	@RequestMapping("/usr/home/main2")
-	@ResponseBody
-	public String showMain2() {
-		return "반갑습니다";
-	}
-	@RequestMapping("/usr/home/main3")
-	@ResponseBody
-	public String showMain3() {
-		return "잘가세요";
-	}
-	@RequestMapping("/usr/home/main4")
-	@ResponseBody
-	public int showMain4() {
-		return count++;
-	}
-	@RequestMapping("/usr/home/main5")
-	@ResponseBody
-	public String showMain5() {
+	
+	public UsrHomeController(){
 		count = 0;
-		return "count의 값이 0으로 초기화 되었습니다.";
+	}
+	@RequestMapping("/usr/home/getCount")
+	@ResponseBody
+	public int getCount() {
+		return count;
+	}
+	@RequestMapping("/usr/home/doSetCount")
+	@ResponseBody
+	public String doSetCount(int count) {
+		this.count = count;
+		return "count의 값이 "+ this.count +"으로 초기화 되었습니다.";
 	}
 	
 }
