bc2a5a58dd295c567e6f7ffe0bd4ec1e2f27d0bc
hyun0547 <hyun0547@gmail.com>
2021-10-20 21:27:46 +0900

    Service 와 Repository 분할

--- src/main/java/com/sbs/exam/demo/service/ArticleService.java
+++ src/main/java/com/sbs/exam/demo/service/ArticleService.java
@@ -1,58 +1,36 @@
 package com.sbs.exam.demo.service;
 
+
 import java.util.ArrayList;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.sbs.exam.demo.repository.ArticleRepository;
 import com.sbs.exam.demo.vo.Article;
 
 @Service
 public class ArticleService {
-		private int lastId;
-		private ArrayList<Article> articles;
-		
-		public ArticleService () {
-			lastId = 0;
-			articles = new ArrayList<>();
-			makeTest();
-		}
-		
-		public void makeTest() {
-			for(int i = 1; i <= 10; i++) {
-				String title = "제목" + i;
-				String body = "내용" + i;
-				doAdd(title, body);
-			}
-		}
-		public Article getArticle(int id) {
-			for(Article article : articles) {
-				if(article.getId() == id) {
-					return article;
-				}
-			}
-			return null;
-		}
-		public ArrayList<Article> getArticles() {
-			if(articles.size() == 0) {
-				return null;
-			}
-			return articles;
-		}
-		public void doAdd(String title, String body) {
-			Article article = new Article(++lastId, title, body);
-			articles.add(article);
-		}
-		public boolean doDelete(int id) {
-			Article article = getArticle(id);
-			articles.remove(article);
-			return article != null;
-		}
-		public boolean doModify(int id, String title, String body) {
-			Article article = getArticle(id);
-			if(article != null) {
-				article.setBody(body);
-				article.setTitle(title);
-			}
-			return article != null;
-		}
+	@Autowired
+	ArticleRepository repository;
+	
+	public ArticleService() {}
+	
+	public ArrayList<Article> getArticles() {
+		return repository.getArticles();
+	}
+	public Article getArticle(int id) {
+		return repository.getArticle(id);
+	}
+	public void doAdd(String title, String body) {
+		repository.doAdd(title, body);
+	}
+	public boolean doDelete(int id) {
+		return repository.doDelete(id);
+	}
+	public boolean doModify(int id, String title, String body) {
+		return repository.doModify(id, title, body);
+	}
+	
+	
 }
