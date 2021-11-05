3fb1e31f9b8f27b329fec7fdddaddbf8dd3940e2
hyun0547 <hyun0547@gmail.com>
2021-11-05 19:07:38 +0900

    jstl 사용 jsp 적용

--- src/main/java/com/sbs/exam/demo/controller/UsrHomeController.java
+++ src/main/java/com/sbs/exam/demo/controller/UsrHomeController.java
@@ -1,6 +1,7 @@
 package com.sbs.exam.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -9,21 +10,14 @@ import lombok.Data;
 @Data
 @Controller
 public class UsrHomeController {
-	private int count = 0;
-	
-	public UsrHomeController(){
-		count = 0;
+	@RequestMapping("/usr/home/main")
+	public String main(Model model) {
+		model.addAttribute("position", "MainPage");
+		return "/usr/home/main";
 	}
-	@RequestMapping("/usr/home/getCount")
-	@ResponseBody
-	public int getCount() {
-		return count;
-	}
-	@RequestMapping("/usr/home/doSetCount")
-	@ResponseBody
-	public String doSetCount(int count) {
-		this.count = count;
-		return "count의 값이 "+ this.count +"으로 초기화 되었습니다.";
+	@RequestMapping("/")
+	public String showMain() {
+		return "redirect:/usr/home/main";
 	}
 	
 }
